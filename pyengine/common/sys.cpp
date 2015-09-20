3977196b0b3a69a41e4ee6360ca96c4dbcd2a1f3
vikatory <vikatory@sina.com>
2015-09-20 15:45:09 +0800

    commit

--- pyengine/common/sys.cpp
+++ pyengine/common/sys.cpp
@@ -4,6 +4,7 @@
 
 #if PE_PLATFORM == PLATFORM_WIN32
 #include <windows.h>
+#include<tchar.h>
 #endif
 
 using namespace std;
@@ -18,6 +19,33 @@ void SetEnviromentValue(string sKey, string sValue)
 	memset(wsValue, 0, sizeof(wsValue));
 	MultiByteToWideChar(CP_ACP, 0, sValue.c_str(), (int)strlen(sValue.c_str()) + 1, wsValue, int(sizeof(wsValue) / sizeof(wsValue[0])));
 	SetEnvironmentVariable(wsKey, wsValue);
+
+	return;
+
+	LPTSTR lpszVariable;
+	LPTCH lpvEnv;
+
+	//获得环境变量内存块的指针
+	lpvEnv = GetEnvironmentStrings();
+	if (lpvEnv == NULL)
+	{
+		printf("GetEnvironmentStrins failed(%d)/n", GetLastError());
+		return ;
+	}
+
+	//环境变量字符串是以NULL分隔的，内存块以NULL结尾
+	lpszVariable = (LPTSTR)lpvEnv;
+
+	while (*lpszVariable)
+	{
+		_tprintf(TEXT("%s/n"), lpszVariable);
+		wcout << lpszVariable << "------------------"<<endl;
+		lpszVariable += lstrlen(lpszVariable) + 1;   //移动指针
+	}
+
+	FreeEnvironmentStrings(lpvEnv);
+
+
 #endif
 }
 
--- pyengine/pyengine.cpp
+++ pyengine/pyengine.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 namespace bf = boost::filesystem;				// 简单别名
-
+namespace python = boost::python;
 
 PyEngine::PyEngine()
 {
@@ -19,19 +19,67 @@ PyEngine::~PyEngine()
 	// Py_Finalize();
 }
 
+
+
+
+
+
+
+#include <boost/detail/lightweight_test.hpp>
+
+
+void eval_test()
+{
+	python::object result = python::eval("'abcdefg'.upper()");
+	std::string value = python::extract<std::string>(result) BOOST_EXTRACT_WORKAROUND;
+	BOOST_TEST(value == "ABCDEFG");
+}
+
+void check_pyerr(bool pyerr_expected = false)
+{
+	if (PyErr_Occurred())
+	{
+		if (!pyerr_expected) {
+			BOOST_ERROR("Python Error detected");
+			PyErr_Print();
+		}
+		else {
+			PyErr_Clear();
+		}
+	}
+	else
+	{
+		BOOST_ERROR("A C++ exception was thrown  for which "
+			"there was no exception handler registered.");
+	}
+}
+
+
+
 void PyEngine::init()
 {
 	bf::path oCurPath = bf::current_path();		// 取得当前目录, 一般是cpp文件夹，若把exe拿出来运行，就是exe所在的文件夹
 	string sCurPath = oCurPath.string();
 	cout << sCurPath << endl;
-	SetEnviromentValue("PYTHONPATH", "E:\\CODE\\project_1\\Python-3.5.0\\Lib");
+	// 通过设置环境变量包含dll路径
+	SetEnviromentValue("PYTHONPATH", "E:\\CODE\\project_1\\python-3.5.0-embed-amd64;E:\\CODE\\project_1\\python-3.5.0-embed-amd64\\Lib");
+	Py_SetProgramName(L"python35");
 	Py_Initialize();
 
+	using namespace boost::python;				// 作用域类有效
+	//object main_module = import("__main__");
+
+//#include <boost/detail/lightweight_test.hpp>
+	python::object result = python::eval("'abcdefg'.upper()");
+	//std::string value = python::extract<std::string>(result) BOOST_EXTRACT_WORKAROUND;
+	////BOOST_TEST(value == "ABCDEFG");
+
+	//object main_module = import("__main__");
+	//if (python::handle_exception(eval_test)) {
+	//	check_pyerr();
+	//}
 	
 }
-
-
-
 
 
 
